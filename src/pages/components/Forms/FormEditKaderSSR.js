7c083177a90cbfd7f395dc51e866dab71515164c
murtisd <murtisari1908@gmail.com>
2024-04-22 11:07:40 +0700

    Menambahkan editan halaman data kader ssr dan button edit hapus

---
+++ src/pages/components/Forms/FormEditKaderSSR.js
@@ -0,0 +1,160 @@
+import React, { useState, useEffect } from "react"; // Tambahkan useEffect setelah diimpor
+import { useRouter } from 'next/router';
+
+export default function CardTableKaderSSR({ kader, handleUpdateDataKader }) {
+    const [formData, setFormData] = useState(kader);
+    const router = useRouter();
+  
+    // Update form data jika prop kader berubah
+    useEffect(() => {
+      setFormData(kader);
+    }, [kader]);
+  
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
+  
+    const handleUpdate = () => {
+      const confirmation = window.confirm("Apakah Anda yakin ingin memperbarui data kader?");
+      if (confirmation) { 
+        router.push('/ssr/kader'); // Redirect kembali setelah berhasil diperbarui
+      }
+    };
+  
+    const handleCancel = () => {
+      router.push('/ssr/kader'); // Kembali tanpa melakukan perubahan
+    };
+    return (
+      <>
+        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0">
+          <div className="rounded-t bg-white mb-0 px-6 py-6">
+            <div className="text-center flex justify-between">
+              <h6 className="text-green-700 text-xl font-bold">
+                Edit Anggota Kader
+              </h6>
+            </div>
+          </div>
+          <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
+          <form>
+            <div className="flex flex-wrap mt-3">
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="selectProvinsi">
+                    Provinsi
+                  </label>
+                  <input
+                    type="text" id="provinsi" name="provinsi"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="selectKota">
+                    Kota
+                  </label>
+                  <input
+                    type="text" id="kota" name="kota"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="selectKecamatan">
+                    Kecamatan
+                  </label>
+                  <input
+                    type="text" id="kecamatan" name="kecamatan"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="noUrut">
+                    No Urut
+                  </label>
+                  <input
+                    type="text" id="noUrut" name="noUrut"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+              </div>
+              <div className="w-full lg:w-6/12 px-4">
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="nama">
+                    Nama
+                  </label>
+                  <input
+                    type="text" id="nama" name="nama"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="nomorIndukAnggota">
+                    Nomor Induk Anggota
+                  </label>
+                  <input
+                    type="text" id="nomorIndukAnggota" name="nomorIndukAnggota"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="jenisKelamin">
+                    Jenis Kelamin
+                  </label>
+                  <select
+                    id="jenisKelamin" name="jenisKelamin"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  >
+                    <option value="Laki-laki">Laki-laki</option>
+                    <option value="Perempuan">Perempuan</option>
+                  </select>
+                </div>
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="tanggalLahir">
+                    Tanggal Lahir
+                  </label>
+                  <input
+                    type="date" id="tanggalLahir" name="tanggalLahir"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="noTelepon">
+                    Nomor Telepon
+                  </label>
+                  <input
+                    type="text" id="noTelepon" name="noTelepon"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+                <div className="relative w-full mb-3">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="alamat">
+                    Alamat
+                  </label>
+                  <input
+                    type="text" id="alamat" name="alamat"
+                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="text-center flex justify-end mr-3 mt-3">
+              <button
+                className="bg-green-700 active:bg-blueGray-600 text-white font-bold text-sm px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={handleUpdate} // Ganti onClick dengan handleUpdate
+              >
+                Simpan
+              </button>
+              <button
+                className="bg-blueGray-400 active:bg-blueGray-600 text-white font-bold text-sm px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                type="button"
+                onClick={handleCancel} // Ganti onClick dengan handleCancel
+              >
+                Batal
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
