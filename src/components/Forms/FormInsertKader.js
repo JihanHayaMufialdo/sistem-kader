25618dbbe42252adceda10d5c15530bdc2c08655
murtisd <murtisari1908@gmail.com>
2024-05-20 20:21:00 +0700

    menambahkan crud data kader

--- src/components/Forms/FormInsertKader.js
+++ src/components/Forms/FormInsertKader.js
@@ -1,37 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from 'next/router';
+import axios from 'axios';
 
-// Function to handle confirmation before creating account
-function handleCreateData(handleSimpanDataKader) {
-  const confirmation = window.confirm("Apakah anda yakin ingin menambahkan data kader?");
-  if (confirmation) {
-    // Handle positive confirmation (account creation logic)
-    console.log("Menambahkan data...");
+const initialFormData = {
+  no_induk: '', 
+  no_urut: '', 
+  nama: '',
+  jenis_kelamin: '',
+  no_telp: '',
+  alamat: '',
+  id_kecamatan: '',
+  provinsi: '',
+  kota: ''
+};
 
-    // Contoh data kader baru yang akan ditambahkan, sesuaikan dengan data yang diinputkan pengguna
-    const dataKaderBaru = {
-      id: 16, // Atur ID secara dinamis sesuai dengan jumlah data kader saat ini
-      nama: "Nama Kader Baru",
-      jenisKelamin: "Laki-laki",
-      tanggalLahir: "1990-01-01", // Contoh tanggal lahir, sesuaikan dengan input pengguna
-      noTelepon: "08123456789",
-      alamat: "Alamat Kader Baru",
-      kecamatan: "Kecamatan Baru",
-      kota: "Kota Baru",
-      provinsi: "Provinsi Baru"
-    };
-
-    // Panggil fungsi untuk menyimpan data kader baru
-    //handleSimpanDataKader(dataKaderBaru);
-  } else {
-    // Handle negative confirmation (do nothing)
-    console.log("Menambahkan data gagal.");
-  }
-}
-
-export default function InsertKader({ handleSimpanDataKader }) {
-
+export default function InsertKader() {
+  const [formData, setFormData] = useState(initialFormData);
   const router = useRouter();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const confirmation = window.confirm("Apakah anda yakin ingin menambahkan data kader?");
+    if (confirmation) {
+      try {
+        const response = await axios.post('http://localhost:8000/kader', formData);
+        console.log(response.data); // Menampilkan respon dari server
+        setFormData(initialFormData);
+        router.push('/admin/kader/');
+      } catch (error) {
+        console.error('Error creating account:', error);
+      }
+    } else {
+      console.log("Menambahkan data gagal.");
+    }
+  };
+
   const handleButtonKembaliClick = () => {
     router.push('/admin/kader/');
   };
@@ -47,42 +55,42 @@ export default function InsertKader({ handleSimpanDataKader }) {
           </div>
         </div>
         <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <form>
+          <form onSubmit={handleSubmit}>
             <div className="flex flex-wrap mt-3">
               <div className="w-full lg:w-6/12 px-4">
                 <div className="relative w-full mb-3">
-                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="selectProvinsi">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="provinsi">
                     Provinsi
                   </label>
                   <input
-                    type="text" id="provinsi" name="provinsi"
+                    type="text" id="provinsi" name="provinsi" value={formData.provinsi} onChange={handleChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   />
                 </div>
                 <div className="relative w-full mb-3">
-                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="selectKota">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="kota">
                     Kota
                   </label>
                   <input
-                    type="text" id="kota" name="kota"
+                    type="text" id="kota" name="kota" value={formData.kota} onChange={handleChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   />
                 </div>
                 <div className="relative w-full mb-3">
-                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="selectKecamatan">
-                    Kecamatan
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="id_kecamatan">
+                    ID Kecamatan
                   </label>
                   <input
-                    type="text" id="kecamatan" name="kecamatan"
+                    type="text" id="id_kecamatan" name="id_kecamatan" value={formData.id_kecamatan} onChange={handleChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   />
                 </div>
                 <div className="relative w-full mb-3">
-                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="noUrut">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="no_urut">
                     No Urut
                   </label>
                   <input
-                    type="text" id="noUrut" name="noUrut"
+                    type="text" id="no_urut" name="no_urut" value={formData.no_urut} onChange={handleChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   />
                 </div>
@@ -93,46 +101,37 @@ export default function InsertKader({ handleSimpanDataKader }) {
                     Nama
                   </label>
                   <input
-                    type="text" id="nama" name="nama"
+                    type="text" id="nama" name="nama" value={formData.nama} onChange={handleChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   />
                 </div>
                 <div className="relative w-full mb-3">
-                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="nomorIndukAnggota">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="no_induk">
                     Nomor Induk Anggota
                   </label>
                   <input
-                    type="text" id="nomorIndukAnggota" name="nomorIndukAnggota"
+                    type="text" id="no_induk" name="no_induk" value={formData.no_induk} onChange={handleChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   />
                 </div>
                 <div className="relative w-full mb-3">
-                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="jenisKelamin">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="jenis_kelamin">
                     Jenis Kelamin
                   </label>
                   <select
-                    id="jenisKelamin" name="jenisKelamin"
+                    id="jenis_kelamin" name="jenis_kelamin" value={formData.jenis_kelamin} onChange={handleChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   >
-                    <option value="Laki-laki">Laki-laki</option>
-                    <option value="Perempuan">Perempuan</option>
+                    <option value="L">Laki-laki</option>
+                    <option value="P">Perempuan</option>
                   </select>
                 </div>
                 <div className="relative w-full mb-3">
-                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="tanggalLahir">
-                    Tanggal Lahir
-                  </label>
-                  <input
-                    type="date" id="tanggalLahir" name="tanggalLahir"
-                    className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  />
-                </div>
-                <div className="relative w-full mb-3">
-                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="noTelepon">
+                  <label className="block uppercase text-green-600 text-xs font-bold mb-2" htmlFor="no_telp">
                     Nomor Telepon
                   </label>
                   <input
-                    type="text" id="noTelepon" name="noTelepon"
+                    type="text" id="no_telp" name="no_telp" value={formData.no_telp} onChange={handleChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   />
                 </div>
@@ -141,27 +140,26 @@ export default function InsertKader({ handleSimpanDataKader }) {
                     Alamat
                   </label>
                   <input
-                    type="text" id="alamat" name="alamat"
+                    type="text" id="alamat" name="alamat" value={formData.alamat} onChange={handleChange}
                     className="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
                   />
                 </div>
               </div>
             </div>
             <div className="text-center flex justify-end mr-3 mt-3">
-            <button
-            className="bg-green-700 active:bg-blueGray-600 text-white font-bold text-sm px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
-            type="submit"
-            onClick={() => handleCreateData(handleSimpanDataKader)}
-          >
-            Simpan
-          </button>
-              <a
+              <button
+                className="bg-green-700 active:bg-blueGray-600 text-white font-bold text-sm px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
+                type="submit"
+              >
+                Simpan
+              </button>
+              <button
                 className="bg-blueGray-400 active:bg-blueGray-600 text-white font-bold text-sm px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                 type="button"
                 onClick={handleButtonKembaliClick}
               >
                 Kembali
-              </a>
+              </button>
             </div>
           </form>
         </div>
